37bb0203028a1bb134b7e32e69c4d6a0a2ec0481
Seminify <Seminify@gmail.com>
2021-05-30 20:20:14 +0900

    onclick 이벤트 시 사진 pdf 실행

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 function open_in_frame(url) {
-    url = './web/viewer.html?file=/docs/' + url
+    var p=document.getElementById('image');
+    p.innerHTML="<img src='images/"+url+".jpg 'width='100%'>";
+    url = './web/viewer.html?file=/docs/' + url+'.pdf';
     $('#my_frame').attr('src', url);
+    
 }
