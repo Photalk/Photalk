8ab996e63488996acf18772fc492343f9acf68f7
shrwhfb <kyg980713@naver.com>
2021-05-30 21:52:20 +0900

    폴더 분리

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 function open_in_frame(url) {
     var p=document.getElementById('image');
-    p.innerHTML="<img src='images/"+url+".jpg 'width='100%'>";
-    url = './web/viewer.html?file=/docs/' + url+'.pdf';
-    $('#PDF').attr('src', url);
+    p.innerHTML="<img src='images/신화/"+url+".jpg 'width='100%'>";
+    url = './web/viewer.html?file=/docs/신화/' + url+'.pdf';
+    $('#my_frame').attr('src', url);
+    
+}
+
+function open_in_frame2(url) {
+    var p=document.getElementById('image');
+    p.innerHTML="<img src='images/전설/"+url+".jpg 'width='100%'>";
+    url = './web/viewer.html?file=/docs/전설/' + url+'.pdf';
+    $('#my_frame').attr('src', url);
+    
+}
+
+function open_in_frame3(url) {
+    var p=document.getElementById('image');
+    p.innerHTML="<img src='images/민담/"+url+".jpg 'width='100%'>";
+    url = './web/viewer.html?file=/docs/민담/' + url+'.pdf';
+    $('#my_frame').attr('src', url);
     
 }
