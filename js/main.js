d63520f3afff42647ca36fa016249a615f8bf8e9
Seminify <Seminify@gmail.com>
2021-05-30 20:35:48 +0900

    크기 수정

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ function open_in_frame(url) {
     var p=document.getElementById('image');
     p.innerHTML="<img src='images/"+url+".jpg 'width='100%'>";
     url = './web/viewer.html?file=/docs/' + url+'.pdf';
-    $('#my_frame').attr('src', url);
+    $('#PDF').attr('src', url);
     
 }
