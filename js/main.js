6e7917fb225fd22946edf810fce0ad0f6f2bd810
Seminify <Seminify@gmail.com>
2021-06-04 16:39:15 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-function open_pdf(type, url) {
-    url = './web/viewer.html?file=/docs/' + type + '/' + url + '.pdf';
-    $('#PDF').attr('src', url);
+function open_pdf(type,url){
+    url = './web/viewer.html?file=/docs/'+type+'/' + url+'.pdf';
+    $('#my_frame').attr('src', url);
 }
-function open_image(type, url) {
-    var p = document.getElementById('image');
-    p.innerHTML = "<img src='images/" + type + "/" + url + ".jpg 'width='100%'>";
+function open_image(type,url){
+    var p=document.getElementById('image');
+    p.innerHTML="<img src='images/"+type+"/"+url+".jpg 'width='100%'>";
 }
-function open_map(lat, long) {
+function open_map(lat,long){
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-        mapOption = {
-            center: new kakao.maps.LatLng(lat, long), // 지도의 중심좌표
-            level: 3, // 지도의 확대 레벨
-            mapTypeId: kakao.maps.MapTypeId.ROADMAP // 지도종류
-        };
+          mapOption = {
+              center: new kakao.maps.LatLng(lat, long), // 지도의 중심좌표
+              level: 3, // 지도의 확대 레벨
+              mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+          };
 
-    // 지도를 생성한다
-    var map = new kakao.maps.Map(mapContainer, mapOption);
+      // 지도를 생성한다
+      var map = new kakao.maps.Map(mapContainer, mapOption);
 }
 
-function click_event(type, url, lat, long) {
-    open_image(type, url);
-    open_map(lat, long)
-    open_pdf(type, url)
+function click_event(type,url,lat,long) {
+    open_image(type,url);
+    open_map(lat,long)
+    open_pdf(type,url)
 }
 
 const getContent = (data) => {
@@ -31,10 +31,10 @@ const getContent = (data) => {
     for (let i = 0; i < content.length; i++) {
 
         var tableRow = {
-            no: i + 1,
-            title: content[i].title,
-            lat: content[i].lat,
-            long: content[i].long
+            no         : i+1,
+            title        : content[i].title,
+            lat   : content[i].lat,
+            long       : content[i].long
         };
 
         tbodyStr += setRow(tableRow);
